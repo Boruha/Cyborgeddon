c41537a7c30f6b2326519634e386878fbf5d213c
Borja Pozo Wals <borja.pozo@gmail.com>
2019-11-14 15:29:27 +0100

    Try 1

---
+++ src/gilardo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <gilardo.h>
+
+void insulto ()
+{
+	std::cout << "SOS UN GILARDO BRO PERO ESTO FUNCIONA BUENARDO" << std::endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "ent/gilardo.h"
 
 int main()
 {
 	std::cout << "FUNCIONO" << std::endl;
+	std::cout << "REPUTASO" << std::endl;
+	insulto();
 	return 0;
 }
