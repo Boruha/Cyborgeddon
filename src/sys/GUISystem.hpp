7afb344ecc039bf6cef7e1e49b5553cb383f8108
srjaguarr <dnavasdiaz97@gmail.com>
2019-12-16 19:03:33 +0100

    .

--- src/sys/GUISystem.hpp
+++ src/sys/GUISystem.hpp
@@ -1,16 +1,7 @@
 #pragma once
+#include <sys/Systems.hpp>
 
-#include <ent/Entities.hpp>
-#include <src/man/EntityManager.hpp>
-
-#include <vector>
-
-struct CollisionSystem
-{
-    //Vamos a hacer diferentes updates: Enemigo con balas, Personaje con balas, Personaje con enemigo, Personaje con pared, etc.
-    void update(std::unique_ptr<EntityPlayer>&, const std::vector<std::unique_ptr<EntityDoor>>&, const std::vector<std::unique_ptr<EntityKey>>&, const std::vector<std::unique_ptr<EntityEnemy>>&, const std::vector<std::unique_ptr<EntityBullet>>& );
-    void update(std::unique_ptr<EntityPlayer>&, const std::vector<std::unique_ptr<EntityDoor>>&) const;
-    void update(std::unique_ptr<EntityPlayer>&, const std::vector<std::unique_ptr<EntityKey>>&)  const;
-    void update(const std::vector<std::unique_ptr<EntityEnemy>>&, const std::vector<std::unique_ptr<EntityBullet>>&)  const;
-    void update(std::unique_ptr<EntityPlayer>&, const std::vector<std::unique_ptr<EntityEnemy>>&)  const;
+struct GUISystem{
+    void init(const Device& device);
+    void update();
 };
