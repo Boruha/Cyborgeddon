b9f339c2e4b9e25a91073dcdf8543b88e02b49bf
Mateo <mateo1198@gmail.com>
2020-01-29 18:17:44 +0100

    Event Receiver preparado para usar el raton. Falta implementar disparo (click izquierdo), orientacion (posicion del raton) y si es necesario cambio de modo (click derecho?)

--- SunlightEngine/EventReceiver.hpp
+++ SunlightEngine/EventReceiver.hpp
@@ -6,15 +6,64 @@
 // TODO : Raton!!!!!!!!!!!!!!!!
 
 namespace Sun {
+	struct Mouse
+	{
+		Mouse() = default;
+
+		Vector2i position;
+		bool leftPressed  { false };
+		bool rightPressed { false };
+	};
+
 	struct EventReceiver : public irr::IEventReceiver {
 		EventReceiver() {
 			std::fill(keys, keys + KEY_CODE_COUNT, false);
 		}
 
-		bool OnEvent(const irr::SEvent &event) override {
-			if (event.EventType == irr::EET_KEY_INPUT_EVENT) {
-				keys[event.KeyInput.Key] = event.KeyInput.PressedDown;
+		bool OnEvent(const irr::SEvent& event) override {
+			switch (event.EventType) {
+
+				case irr::EET_MOUSE_INPUT_EVENT :
+
+					switch(event.MouseInput.Event) {
+
+						case irr::EMIE_LMOUSE_PRESSED_DOWN :
+							mouse.leftPressed = true;
+							break;
+
+						case irr::EMIE_LMOUSE_LEFT_UP :
+							mouse.leftPressed = false;
+							break;
+
+						case irr::EMIE_RMOUSE_PRESSED_DOWN :
+							mouse.rightPressed = true;
+							break;
+
+						case irr::EMIE_RMOUSE_LEFT_UP :
+							mouse.rightPressed = false;
+							break;
+
+						case irr::EMIE_MOUSE_MOVED :
+							mouse.position.x = event.MouseInput.X;
+							mouse.position.y = event.MouseInput.Y;
+							break;
+
+						default:
+							// No usamos la rueda de momento, ni los botones auxiliares
+							break;
+					}
+
+					break;
+
+				case irr::EET_KEY_INPUT_EVENT :
+
+					keys[event.KeyInput.Key] = event.KeyInput.PressedDown;
+
+					break;
+
+				default : break;
 			}
+
 			return false;
 		}
 
@@ -22,7 +71,11 @@ namespace Sun {
 			return keys[keyCode];
 		}
 
+		[[nodiscard]] const Mouse& getMouse() const { return mouse; }
+
 	private:
-		bool keys[KEY_CODE_COUNT] {  };
+
+		bool keys [KEY_CODE_COUNT] {  };
+		Mouse mouse;
 	};
 } // namespace Sun
