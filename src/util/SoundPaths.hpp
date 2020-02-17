4e584ca8320b43629c42356190206dfdf404fc0f
Juanma <pokejuanma@gmail.com>
2020-02-17 17:31:56 +0100

    Cambios SoundPath

--- src/util/SoundPaths.hpp
+++ src/util/SoundPaths.hpp
@@ -4,19 +4,21 @@ constexpr char const * MASTER_STRINGS_BANK 		= "./resources/sounds/banks/Master.
 
 // SOUND EVENTS
 //PLAYER
-constexpr char const * DEMON_SHOOT_EVENT		= "event:/disparo_demon";
-constexpr char const * ANGEL_SHOOT_EVENT		= "event:/disparo_angel";
+constexpr char const * DEMON_SHOOT_EVENT		= "event:/player/disparo_demon";
+constexpr char const * ANGEL_SHOOT_EVENT		= "event:/player/disparo_angel";
 
-constexpr char const * DEMON_CHANGE_EVENT		= "event:/change_demon";
-constexpr char const * ANGEL_CHANGE_EVENT		= "event:/change_angel";
+constexpr char const * DEMON_CHANGE_EVENT		= "event:/player/change_demon";
+constexpr char const * ANGEL_CHANGE_EVENT		= "event:/player/change_angel";
 
-constexpr char const * DASH_PLAYER_EVENT             = "event:/dash_player";
+constexpr char const * DASH_PLAYER_EVENT        = "event:/player/dash_player";
+
+constexpr char const * PICKUP_KEY_EVENT         = "event:/player/pickup_key";
 
 //MUSIC
-constexpr char const * BACKGROUND_MUSIC_EVENT 	= "event:/main_music";
+constexpr char const * BACKGROUND_MUSIC_EVENT 	= "event:/music/main_music";
 
 //ENEMY
-constexpr char const * ASSEMBLED_ATTACK_EVENT   = "event:/assembled_attack";
+constexpr char const * ASSEMBLED_ATTACK_EVENT   = "event:/enemies/assembled_attack";
 
 //AMBIENT
 
