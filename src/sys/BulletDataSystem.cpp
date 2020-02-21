28d3621a46e058602612cad635b4d27194bcff7f
Mateo <mateo1198@gmail.com>
2020-02-21 23:43:52 +0100

    Previo merge nuevas colisiones con mensajes

--- src/sys/BulletDataSystem.cpp
+++ src/sys/BulletDataSystem.cpp
@@ -3,7 +3,7 @@
 
 void BulletDataSystem::update(const std::unique_ptr<GameContext> &context, const float deltaTime) {
     // Mueren balas ?
-    for (auto& bullet : std::get<vector<BulletData>>(context->getComponents(BULLET_DATA_TYPE)))
+    for (auto& bullet : context->getComponents().get<BulletData>())
         if (bullet && !greater_e((bullet.distance_left -= bullet.speed * deltaTime), 0))
             deathMessages.emplace_back(bullet.getEntityID());
 }
