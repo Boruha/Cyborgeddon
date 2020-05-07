2b0dbd1bfac46e188a3224a15e925048e5a7400f
mateo1198 <mateo1198@gmail.com>
2020-05-07 15:02:56 +0200

    Bug cambio de opciones antes de que aparezcan, corregido

--- src/sys/IntroInputSystem.cpp
+++ src/sys/IntroInputSystem.cpp
@@ -3,10 +3,13 @@
 
 
 void IntroInputSystem::fixedUpdate(const Context &context, const float deltaTime) {
-	auto& menu_options = context->getComponents().getComponents<MenuOption>()[0];
 
-	if (context->isKeyTextInput(KEY_LEFT))
-		menu_options.option = (menu_options.option - 1) % menu_options.maxOptions;
-	if (context->isKeyTextInput(KEY_RIGHT))
-		menu_options.option = (menu_options.option + 1) % menu_options.maxOptions;
+	if (context->getVideoIndex() > 0) {
+		auto& menu_options = context->getComponents().getComponents<MenuOption>()[0];
+
+		if (context->isKeyTextInput(KEY_LEFT))
+			menu_options.option = (menu_options.option - 1) % menu_options.maxOptions;
+		if (context->isKeyTextInput(KEY_RIGHT))
+			menu_options.option = (menu_options.option + 1) % menu_options.maxOptions;
+	}
 }
