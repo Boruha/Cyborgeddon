ff2c7f6c6e8f61ee48054fb0a75da42897d9cfb5
Borja Pozo Wals <borja.pozo@gmail.com>
2020-05-22 01:45:00 +0200

    .hpp con todas las mesh y animaciones

---
+++ src/util/MeshPaths.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string_view>
+
+/* DEX */
+constexpr std::string_view ANIMATED_DEX_WALKING  = "../resources/models/Dex/Walking";
+
+/* BOT */
+constexpr std::string_view ANIMATED_BOT_STATIC   = "../resources/models/Bot/Static";
+
+/* ANGEL */
+constexpr std::string_view ANIMATED_ANGEL_STATIC = "../resources/models/Angel/Static";
+
+/* DEMON */
+constexpr std::string_view ANIMATED_DEMON_STATIC = "../resources/models/Demon/Static";
+
+/* CITY */
+constexpr std::string_view MESH_CITY      = "../resources/models/Ciudad/ciudad.obj";
+constexpr std::string_view COLISIONS_CITY = "../resources/models/Ciudad/colisiones.obj";
+
+/* BULLET */
+constexpr std::string_view ANIMATED_BALA   = "../resources/models/Objetos/Bala/BalaCentro";
+
+/* DOOR */
+constexpr std::string_view MESH_DOOR_CLOSE = "../resources/models/Objetos/Puerta/0.obj";
+
+/* KEYS */
+constexpr std::string_view MESH_KEY_DEMON  = "../resources/models/Objetos/Llave_demoniaca/0.obj";
